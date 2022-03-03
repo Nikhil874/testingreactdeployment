5b5127a5c2433a8217f20cffd81c72aec018ce7c
Nikhil874 <95832751+Nikhil874@users.noreply.github.com>
2022-03-03 21:13:42 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const mos=["Andriod","Blackberry","iPhone","Windows Pnone"];
+const mm=["Samsung","HTC","Micromax","Apple"];
+  return <div className="main"><h2 className="redtext">Mobile Operating System</h2>
+  <ul>{mos.map((el)=>{
+    return <OsDisplay system= {el}/>
+  })}</ul>
+  <h2 className="redtext">Mobile Manufacturers</h2>
+  <ul>{mm.map((el1)=>{
+    return <OsDisplay system={el1}/>
+  })}</ul>
+  </div>;
+}
+
+function OsDisplay({system}){
+  console.log(system);
+return<li className="tealtext">{system}</li>
 }
 
 export default App;
